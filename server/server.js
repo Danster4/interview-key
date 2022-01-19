40fed8fc8057966da7b8f1a0fb8e231e7d45fb1a
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-01-18 23:25:03 -0500

    commented out some server.js code until client folder added later

--- server/server.js
+++ server/server.js
@@ -47,9 +47,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// commented out for now, until client folder added
+// app.get('*', (req, res) => {
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
