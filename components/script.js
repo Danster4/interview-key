3216408e7433118e5be2f78d02182e0501e48543
unknown <viakeegan@gmail.com>
2022-01-26 17:53:30 -0500

    start login script

---
+++ components/script.js
@@ -0,0 +1,15 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const Wrapper = () => /*#__PURE__*/
+React.createElement("div", null, /*#__PURE__*/
+React.createElement(Header, null), /*#__PURE__*/
+React.createElement(Auth, null));
+
+
+
+const Header = () => /*#__PURE__*/
+React.createElement("section", { className: "hero is-info is-link" }, /*#__PURE__*/
+React.createElement("div", { className: "hero-body" }, /*#__PURE__*/
+React.createElement("div", { className: "container" }, /*#__PURE__*/
+React.createElement("h1", { className: "title" }, "Interview Key"), /*#__PURE__*/
+
+
+React.createElement("h2", { className: "subtitle" }, "With React + Bulma + Font Awesome"))));
