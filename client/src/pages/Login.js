45f6c24c14cb64d0b7fc869d2a9e88fb64a03543
unknown <viakeegan@gmail.com>
2022-01-29 00:33:06 -0500

    import bulma

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import 'bulma/css/bulma.css';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
