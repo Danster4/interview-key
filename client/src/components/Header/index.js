88301400a6bd46c0ef1f27e9e5806b001579f79b
unknown <viakeegan@gmail.com>
2022-01-29 03:23:57 -0500

    feature/login-2

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = () => {
     Auth.logout();
   }
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-white mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Interview Key 🔑 </h1>
