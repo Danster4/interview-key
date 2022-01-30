d4560311900e253c647041dc980ca401868ead6b
DoodleBob <riley.foust18@gmail.com>
2022-01-29 20:34:04 -0500

    adjusted color and error log on signup

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-primary p-4">
       <div className="container">&copy;2022 by Interview Key</div>
     </footer>
   );
