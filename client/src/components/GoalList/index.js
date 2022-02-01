1a58ed1a7cbcc72513ad1de8675b73ae0265b313
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-02-01 13:10:42 -0500

    updated removeGoal to remove cache and show deletion immediately on front-end without hard reload

--- client/src/components/GoalList/index.js
+++ client/src/components/GoalList/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useMutation } from '@apollo/client';
 import { Redirect } from 'react-router-dom';
 import { REMOVE_GOAL } from '../../utils/mutations';
+import { QUERY_GOALS, QUERY_ME } from '../../utils/queries';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
@@ -23,7 +24,12 @@ const GoalList = ({ goals, username }) => {
 
     try {
       await removeGoal({
-        variables: { goalId }
+        variables: { goalId },
+        update(cache) {
+          const normalizedId = cache.identify({ goalId, __typename: 'Goal' });
+          cache.evict({ goalId: normalizedId });
+          cache.gc();
+        }
       });
 
       <Redirect to="/dashboard" />
@@ -57,3 +63,42 @@ const GoalList = ({ goals, username }) => {
 };
 
 export default GoalList;
+
+// ,
+//         update(cache) {
+//           cache.modify({
+//             fields: {
+//               goals(currentGoalsRefs, { readField }) {
+//                 return currentGoalsRefs.filter(
+//                   goalRef => goalId !== readField('goalId', goalRef),
+//                 )
+//               }
+//             }
+//           })
+//         }
+
+
+
+// const [removeGoal, { error }] = useMutation(REMOVE_GOAL, {
+//   update(cache, { data: { addGoal } }) {
+//     try {
+//       // read what's currently in the cache
+//       const { goals } = cache.readQuery({ query: QUERY_GOALS });
+
+//       // prepend the newest thought to the front of the array
+//       cache.writeQuery({
+//         query: QUERY_GOALS,
+//         data: { goals: [addGoal, ...goals] }
+//       });
+//     } catch (e) {
+//       console.error(e)
+//     }
+
+//     // update me object's cache, appending new thought to the end of the array
+//     const { me } = cache.readQuery({ query: QUERY_ME });
+//     cache.writeQuery({ 
+//       query: QUERY_ME,
+//       data: { me: { ...me, goals: [...me.goals, addGoal] } }
+//     });
+//   }
+// });
--- client/src/components/InterviewForm/index.js
+++ client/src/components/InterviewForm/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { ADD_INTERVIEW } from '../../utils/mutations';
+import { QUERY_INTERVIEWS, QUERY_ME } from '../../utils/queries';
+
 import { useMutation } from '@apollo/client';
 
 const InterviewForm = ({ goalId }) => {
@@ -7,6 +9,7 @@ const InterviewForm = ({ goalId }) => {
   const [interviewLocation, setLocation] = useState('');
   const [interviewTime, setTime] = useState('');
   const [interviewDate, setDate] = useState('');
+  
   const [addInterview, { error }] = useMutation(ADD_INTERVIEW);
 
 
@@ -100,3 +103,28 @@ const InterviewForm = ({ goalId }) => {
 };
 
 export default InterviewForm;
+
+
+// const [addInterview, { error }] = useMutation(ADD_INTERVIEW, {
+//   update(cache, { data: { addInterview } }) {
+//     try {
+//       // read what's currently in the cache
+//       const { interviews } = cache.readQuery({ query: QUERY_INTERVIEWS });
+
+//       // prepend the newest thought to the front of the array
+//       cache.writeQuery({
+//         query: QUERY_INTERVIEWS,
+//         data: { interviews: [addInterview, ...interviews] }
+//       });
+//     } catch (e) {
+//       console.error(e)
+//     }
+
+//     // update me object's cache, appending new thought to the end of the array
+//     const { me } = cache.readQuery({ query: QUERY_ME });
+//     cache.writeQuery({ 
+//       query: QUERY_ME,
+//       data: { me: { ...me.goals, interviews: [...me.goals.interviews, addInterview] } }
+//     });
+//   }
+// });
