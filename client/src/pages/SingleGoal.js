cc423ab388e92a4c4e0865738ca2736bcafae09b
nhodges <hodges27@outlook.com>
2022-02-02 19:40:30 -0500

    goal updated

--- client/src/pages/SingleGoal.js
+++ client/src/pages/SingleGoal.js
@@ -6,24 +6,28 @@ const SingleGoal = (props) => {
   const { goal, username } = props.location.state
 
   return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-           
-          </span>{' '}
-          This goal was created on {goal.createdAt}
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          {goal.goalName}
         </p>
-        <div className="card-body">
-          <p>{goal.goalName}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <p className="title">
+            <span style={{ fontWeight: 700 }} className="text-light">
+             
+            </span>{' '}
+            This goal was created on {goal.createdAt}
+          </p>
         </div>
       </div>
-
-      {goal.interviews.length > 0 && <InterviewList interviews={goal.interviews} username={username} goalId={goal._id}/>}
-      <div className="col-12 mb-3">
-        <InterviewForm  goalId={goal._id}/>
-      </div>
-        
+      <footer class="card-footer">
+        {goal.interviews.length > 0 && <InterviewList interviews={goal.interviews} username={username} goalId={goal._id}/>}
+        <div className="col-12 mb-3">
+          <InterviewForm  goalId={goal._id}/>
+        </div>
+      </footer>
     </div>
 
     
