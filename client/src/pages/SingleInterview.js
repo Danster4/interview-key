26bfaf121c7c8ce9098c77cb7711c7798b3ca2c8
nhodges <hodges27@outlook.com>
2022-02-03 23:24:25 -0500

    updated

--- client/src/pages/SingleInterview.js
+++ client/src/pages/SingleInterview.js
@@ -57,7 +57,7 @@ const SingleInterview = (props) => {
       <form class="box">
         <div class="field">
           <h1>
-            Hey {username}
+            Hello {username}
           </h1>
           <p className="card-header">
             <span style={{ fontWeight: 700 }} className="text-light">
